5d8900ab502750ca6157ffad8c9015f9e01010b4
zoyoshida <zophia.yoshida@outlook.com>
2025-02-03 17:28:12 +0100

    RAAAAAAAH

--- script.js
+++ script.js
@@ -172,6 +172,7 @@ const audioPlayer = (() => {
       clone.querySelector(".track").setAttribute("id", `track-${index}`);
       clone.querySelector(".track-title").textContent = track.title;
       clone.querySelector(".track-artist").textContent = track.artist;
+      clone.querySelector(".playlist-cover").src = track.cover;
       clone.querySelector(".track").addEventListener("click", () => {
         currentTrackIndex = index;
         loadTrack(currentTrackIndex);
